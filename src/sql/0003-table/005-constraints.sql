88cb142dfa906a35f5ffef483c5ccfccf67b41d9
Subash <subashsuni55@gmail.com>
2026-02-02 16:41:40 +0530

    added foreign key constraint

--- src/sql/0003-table/005-constraints.sql
+++ src/sql/0003-table/005-constraints.sql
@@ -19,3 +19,52 @@ create table products(
     discounted_price numeric CHECK (discounted_price > 0),
     CHECK ( price > discounted_price)
 )
+
+-- Not-Null Constraints
+
+create table products(
+    product_no integer not null,
+    name text not null,
+    price numeric
+)
+
+create table products(
+    product_no integer not null,
+    name text not null,
+    price numeric not null Check (price > 0)
+)
+
+-- unique constraints
+
+create table products(
+    product_no integer unique,
+    name text,
+    price numeric
+)
+
+-- primary keys
+
+create table products(
+    product_no integer primary key,
+    name text,
+    price numeric
+)
+
+-- foreign keys
+
+create table products(
+    product_no integer primary key,
+    name text,
+    price numeric
+)
+create table orders(
+    order_id integer primary key,
+    product_no integer references products(product_no),
+    quantity integer
+)
+-- or
+create table orders(
+    order_id integer primary key,
+    product_no integer references products,
+    quantity integer
+)
